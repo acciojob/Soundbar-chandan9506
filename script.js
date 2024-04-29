a416842ff44fbaa33466e80ca20f13aeb88b7665
chandan9506 <152258839+chandan9506@users.noreply.github.com>
2024-04-29 11:52:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,32 @@
-//your JS code here. If required.
+const audios = document.querySelectorAll("audio");
+let buttonsArray = document.getElementsByClassName("btn");
+let stopBtn = document.getElementsByClassName("stop");
+
+
+const buttons = Array.from(buttonsArray);
+
+
+buttons.forEach((button,indexOfBtn) => {
+    button.addEventListener("click",() => {
+        playAudio(indexOfBtn);
+    });
+});
+
+
+function playAudio(indexOfBtn){
+
+    audios.forEach((audio,indexOfAudio) =>{
+        if (indexOfAudio !== indexOfBtn){
+            audio.pause();
+        }
+       
+    });
+    audios[indexOfBtn].play();
+}
+
+
+stopBtn[0].addEventListener("click",function(){
+    audios.forEach(audio =>{
+        audio.pause();
+    });
+});
